11600c52df7db91e40db83342a1b3d27c23fbaf1
ivallee <isaacvallee@gmail.com>
2018-01-23 15:31:01 -0800

    build axios request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '040a694ca0bd6c9af8e4cddf3bee1a3c';
 const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,8 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},ca`;
+  const request = axios.get(url);
   
   return {
     type: FETCH_WEATHER,
+    payload: request
   };
 }
