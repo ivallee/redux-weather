abd2791b8a72371e10247433aec036a253445040
ivallee <isaacvallee@gmail.com>
2018-01-23 15:55:13 -0800

    action fetches weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '040a694ca0bd6c9af8e4cddf3bee1a3c';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
