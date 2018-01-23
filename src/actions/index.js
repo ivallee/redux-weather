89dff09c26828dabe5494f4a2ace6a3f5aeefe28
ivallee <isaacvallee@gmail.com>
2018-01-23 15:27:00 -0800

    init  fetchWeather action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '040a694ca0bd6c9af8e4cddf3bee1a3c';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},ca`;
+  
+  return {
+    type: FETCH_WEATHER,
+  };
+}
