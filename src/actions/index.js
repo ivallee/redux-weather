7e2cb234bd5c7544f98806dcb93a3536ba9e03be
ivallee <isaacvallee@gmail.com>
2018-01-23 21:36:33 -0800

    add comments for redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},ca`;
   const request = axios.get(url);
-  
+  // redux-promise stops the action and resolves the promise
+  // before sending the action with the request data
   return {
     type: FETCH_WEATHER,
     payload: request
