dcd406c725a17e48574355c060f6031550367387
ivallee <isaacvallee@gmail.com>
2018-01-23 22:54:32 -0800

    add reference line

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default ({ data, color }) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={data}>
         <SparklinesLine color={color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
     </div>
   );
